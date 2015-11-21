d889b844a1bd9d2a863def2b8de4351602564224
Sandra Saez <san.saezr@gmail.com>
2015-11-21 18:35:49 +0100

    Add old & new version. The new version is the same but cleaner

--- SSD/system_machines.py
+++ SSD/system_machines.py
@@ -1,55 +1,70 @@
 import simpy
 import random
 
-SIM_TIME = 1000
-RANDOM_SEED = 42
-MEAN = 1.8
-VAR = 0.2
-NUM_MACHINES = 10
-REPOSITORY = 3
-NUM_REPAIRMEN = 3
-MAX_EXPERIENCE = 1.65
-MIN_EXPERIENCE = 0.55
+#### VARIABLES GLOBALES ###
+
+SIM_TIME = 1000 # Tiempo de simulación
+RANDOM_SEED = 42 # Semilla para la funciones de random
+MEAN = 1.8 # Media de la Normal
+VAR = 0.2  # Varianza de la Normal
+NUM_MACHINES = 10 # Numero de maquinas en el sistema
+REPOSITORY = 3 # Numero de maquinas en la reserva
+NUM_REPAIRMEN = 3 # Numero de operarios
+MAX_EXPERIENCE = 1.65 # Experiencia del operario al inciar la solucion
+MIN_EXPERIENCE = 0.55 # Experiencia del operario al acabar la simulación
 
 # Constante que calcula la experiencia del operario
 EXPERIENCE_PER_HOUR = (MAX_EXPERIENCE-MIN_EXPERIENCE)/SIM_TIME
 
-class SystemMachine(object):
-    def __init__(self, env):
-        self.env = env
-        self.machines = simpy.Resource(env, NUM_MACHINES)
-        self.repairmen = simpy.Resource(env, NUM_REPAIRMEN)
-        
-def machine(env, name, system):
-    # Espera hasta que se ocupar un recurso cuando esta disponible
-    # y lo libera cuando lo deja (automaticamente)
-    print('%s arrives to the system at %.2f.' % (name, env.now))
-    while True:
-        with system.machines.request() as request:
-            yield request
-            print('%s working at %.2f.' % (name, env.now))
-            yield env.timeout(random.normalvariate(MEAN, VAR))
-            print('%s has failed at %.2f.' % (name, env.now))
+# Tiempo que tarda en fallar una maquina ~ N(1.8, 0.2)
+def time_to_fail():
+        return random.normalvariate(MEAN, VAR)
 
-        with system.repairmen.request() as request:
-            yield request
-        
-            #yield env.process(repairman(env, system))
-            paramExponential = (EXPERIENCE_PER_HOUR*env.now)+MIN_EXPERIENCE
-            yield env.timeout(random.expovariate(paramExponential))
-            print('%s repaired at %.2f.' % (name, env.now))
+class Machine(object):
+    # Constructor
+    def __init__(self, env, name, system, repairman):
+        self.env = env
+        self.m_name = name
+
+        # Se lanza el proceso que define el comportamiento de una máquina
+        env.process(self.working(system, repairman))
+
+    def time_to_fail(self):
+        return random.normalvariate(MEAN, VAR)
+
+    def working(self, system, repairman):
+ 
+        while True:
+            # Espera hasta que ocupa un recurso cuando esta disponible
+            # y lo libera cuando lo deja (automaticamente)
+            with system.request() as request:
+                yield request        
+                print('%s working at %.2f.' % (self.m_name, self.env.now))
+                yield env.timeout(self.time_to_fail())
+                print('%s has failed at %.2f.' % (self.m_name, env.now))
+
+            # Una vez que se ha roto debe esperar a que haya algun operario libre
+            with repairman.request() as request:
+                yield request
+                paramExponential = (EXPERIENCE_PER_HOUR*self.env.now)+MIN_EXPERIENCE
+                yield self.env.timeout(random.expovariate(paramExponential))
+                print('%s repaired at %.2f.' % (self.m_name, self.env.now))
+
                 
 # Create an environment and start the setup process
+print('System started')
 random.seed(RANDOM_SEED)
 env = simpy.Environment()
 
-system = SystemMachine(env)
+# Creamos dos recursos: el sistema con espacio para diez máquinas y los tres operarios
+system = simpy.Resource(env, capacity=NUM_MACHINES)
+repairman = simpy.Resource(env, capacity=NUM_REPAIRMEN)
 
-# Create 13 machines
+# Se crean 13 maquinas
 total_machines = NUM_MACHINES + REPOSITORY
 for i in range(total_machines):
-    env.process(machine(env, 'Machine %d' % i, system))
+    Machine(env, 'Machine %d' % i, system, repairman)
 
-# Execute!
+# Ejecuta la simulacion
 env.run(until=SIM_TIME)
     
